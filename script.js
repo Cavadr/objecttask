224bfa348c368700b9018127b0df4a2f39b6efab
Cavadr <javadir@code.edu.az>
2021-12-01 06:19:29 +0400

    objects task

---
+++ script.js
@@ -0,0 +1,37 @@
+// let user={
+//     name:"Cavad",
+//     surname:"Rustamli",
+//     age: 25
+// }
+
+// let user1={
+//     name1:"Cavid",
+//     surname1:"Cavidov",
+//     age1: 18
+// }
+
+// let user2={
+//     name2:"Camal",
+//     surname2:"Camalov",
+//     age2: 31
+// }
+
+// let Group=Object.assign(user,user1,user2);
+
+//foreach
+
+// Group.forEach(myFunction);
+// console.log(Group);
+
+// function myFunction(value,) {
+//     txt += value + "<br>"; 
+//   }
+
+
+// const numbers2 = Group.age.map(myFunction);
+
+// function myFunction(value,) {
+//     return value * 2;
+//   }
+
+//   console.log(numbers2)
